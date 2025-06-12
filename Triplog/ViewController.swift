3cef9cb0be65c77a11d1da2d9f966ac136dd0740
subitt0 <enbiness@gmail.com>
2025-06-12 14:45:51 +0900

    Initial commit

---
+++ Triplog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Triplog
+//
+//  Created by 배수빈 on 6/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TriplogTests/TriplogTests.swift
@@ -0,0 +1,17 @@
+//
+//  TriplogTests.swift
+//  TriplogTests
+//
+//  Created by 배수빈 on 6/12/25.
+//
+
+import Testing
+@testable import Triplog
+
+struct TriplogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
